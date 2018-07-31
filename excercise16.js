d8342362ef34251bea295b5f09c1e7899cc47a17
Carlos Garcia <cgarciapieto@gmail.com>
2018-07-31 13:24:02 -0500

    javascript-classwork6-functions2-cgarciapieto.iml

---
+++ excercise16.js
@@ -0,0 +1,17 @@
+var favoriteNumber = parseInt(prompt("enter your favorite Number"));
+alert("your favorite number is " +  favoriteNumber);
+
+
+function aboveOrBelow100 (){}
+
+if (favoriteNumber < 100)
+{
+    alert("number is below 100")
+}
+
+else (favoriteNumber = 100);
+{
+    alert("It's 100")
+}
+
+aboveOrBelow100();
